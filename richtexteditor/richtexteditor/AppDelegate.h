b11229cc5db07571bbf654e5cd170a23231451f1
Nikolay Ilin <na.iline@me.com>
2015-03-11 15:15:49 +0300

    init

---
+++ richtexteditor/richtexteditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  richtexteditor
+//
+//  Created by Nikolay Ilin on 11.03.15.
+//  Copyright (c) 2015 Soft-Artel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ richtexteditor/richtexteditor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  richtexteditor
+//
+//  Created by Nikolay Ilin on 11.03.15.
+//  Copyright (c) 2015 Soft-Artel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
